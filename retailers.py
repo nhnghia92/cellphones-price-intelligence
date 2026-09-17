53290b64e481fd55f504c4dae989113954b6218b
nhnghia92 <nhnghia92@gmail.com>
2026-09-17 10:03:00 +0700

    Create retailers.py

---
+++ retailers.py
@@ -0,0 +1,52 @@
+RETAILERS = {
+    "RET_001": {
+        "name": "Thế Giới Di Động",
+        "code": "tgdd",
+        "active": True,
+    },
+    "RET_002": {
+        "name": "Điện Máy Xanh",
+        "code": "dienmayxanh",
+        "active": True,
+    },
+    "RET_003": {
+        "name": "TOPZone",
+        "code": "topzone",
+        "active": True,
+    },
+    "RET_004": {
+        "name": "F.Studio",
+        "code": "fstudio",
+        "active": True,
+    },
+    "RET_005": {
+        "name": "CellphoneS",
+        "code": "cellphones",
+        "active": True,
+    },
+    "RET_006": {
+        "name": "Hoàng Hà Mobile",
+        "code": "hoanghamobile",
+        "active": True,
+    },
+    "RET_007": {
+        "name": "Minh Tuấn Mobile",
+        "code": "minhtuanmobile",
+        "active": True,
+    },
+    "RET_008": {
+        "name": "ShopDunk",
+        "code": "shopdunk",
+        "active": True,
+    },
+    "RET_009": {
+        "name": "Di Động Việt",
+        "code": "didongviet",
+        "active": True,
+    },
+    "RET_010": {
+        "name": "Shopee",
+        "code": "shopee",
+        "active": True,
+    },
+}
